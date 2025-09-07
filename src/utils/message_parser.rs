6fe9018685c1b1ed62d3e00b3ef605b831c690e5
shawtygold <mylnikovmisha@gmail.com>
2025-09-07 10:26:05 +0500

    [refactor] add error propagation

--- src/utils/message_parser.rs
+++ src/utils/message_parser.rs
@@ -1,21 +1,22 @@
+use anyhow::Error;
 use regex::Regex;
 
-pub fn extract_sol_token_address(msg: &str) -> Option<String> {
-    let re = Regex::new("[1-9A-HJ-NP-Za-km-z]{32,44}").unwrap();
+pub fn extract_sol_token_address(msg: &str) -> Result<Option<String>, Error> {
+    let re = Regex::new("[1-9A-HJ-NP-Za-km-z]{32,44}")?;
 
     if let Some(caps) = re.captures(msg) {
-        return Some(caps[0].to_string());
+        return Ok(Some(caps[0].to_string()));
     }
 
-    None
+    Ok(None)
 }
 
-pub fn extract_token_symbol(msg: &str) -> Option<String> {
-    let re = Regex::new("\\$[a-zA-Z]+").unwrap();
+pub fn extract_token_symbol(msg: &str) -> Result<Option<String>, Error> {
+    let re = Regex::new("\\$[a-zA-Z]+")?;
 
     if let Some(caps) = re.captures(msg) {
-        return Some(caps[0].to_string());
+        return Ok(Some(caps[0].to_string()));
     }
 
-    None
+    Ok(None)
 }
