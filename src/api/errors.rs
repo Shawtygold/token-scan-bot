fbbe6905adf03735a2326c6d320b123a34f1b20f
shawtygold <mylnikovmisha@gmail.com>
2025-09-07 10:01:58 +0500

    [refactor] add new Api Errors

--- src/api/errors.rs
+++ src/api/errors.rs
@@ -1,7 +1,51 @@
+use serde::Deserialize;
 use thiserror::Error;
 
-#[derive(Debug, Error)]
-pub enum ApiErrors {
-    #[error("Missing token data: {0}")]
-    MissingData(String),
+#[derive(Error, Debug)]
+pub enum ApiError {
+    #[error("{} | Bad Request: status: {}, {}", error_data.source, error_data.status_code, error_data.message)]
+    BadRequest { error_data: ApiErrorData },
+    #[error("{} | Unauthorized: status: {}, {}", error_data.source, error_data.status_code, error_data.message)]
+    Unauthorized { error_data: ApiErrorData },
+    #[error("{} | Forbidden: status: {}, {}", error_data.source, error_data.status_code, error_data.message)]
+    Forbidden { error_data: ApiErrorData },
+    #[error("{} | TooManyRequests: status: {}, {}", error_data.source, error_data.status_code, error_data.message)]
+    TooManyRequests { error_data: ApiErrorData },
+    #[error("{} | InternalServerError: status: {}, {}", error_data.source, error_data.status_code, error_data.message)]
+    InternalServerError { error_data: ApiErrorData },
+    #[error("{} | Service Unavailable: status: {}, {}", error_data.source, error_data.status_code, error_data.message)]
+    ServiceUnavailable { error_data: ApiErrorData },
+    #[error("{} | Access Denied: status: {}, {}", error_data.source, error_data.status_code, error_data.message)]
+    AccessDenied { error_data: ApiErrorData },
+    #[error("{} | Missing API Key: status: {}, {}", error_data.source, error_data.status_code, error_data.message)]
+    MissingAPIKey { error_data: ApiErrorData },
+    #[error("{} | Invalid API Key: status: {}, {}", error_data.source, error_data.status_code,error_data.message)]
+    InvalidAPIKey { error_data: ApiErrorData },
+    #[error("{} | Not Found: status: {}, {}", error_data.source, error_data.status_code, error_data.message)]
+    NotFound { error_data: ApiErrorData },
+    #[error("{} | Bad Gateway: status: {}, {}", error_data.source, error_data.status_code, error_data.message)]
+    BadGateway { error_data: ApiErrorData },
+    #[error("{} | Gateway Timeout: status: {}, {}", error_data.source, error_data.status_code, error_data.message)]
+    GatewayTimeout { error_data: ApiErrorData },
+    #[error("{} | Unknown: {}, {}", error_data.source, error_data.status_code, error_data.message)]
+    Unknown { error_data: ApiErrorData },
+}
+
+#[derive(Debug, Clone)]
+pub struct ApiErrorData {
+    pub source: String,
+    pub status_code: u16,
+    pub message: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct MoralisApiErrorData {
+    #[serde(rename = "statusCode")]
+    pub status_code: u16,
+    pub message: String
+}
+
+#[derive(Debug, Deserialize)]
+pub struct JupiterApiErrorData {
+    pub error: String,
 }
